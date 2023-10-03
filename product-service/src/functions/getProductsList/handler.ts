4abee4b5b7aebd878e79c5eabcb67f8b38289e3b
Mykyta_Kurylko <mykyta_kurylko@epam.com>
2023-10-03 21:39:17 +0200

    Task 3 (First API with AWS API Gateway and AWS Lambda)

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,14 +1,11 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
-
 import { formatJSONResponse } from '@libs/api-gateway';
 import { middyfy } from '@libs/lambda';
 
 import { products } from '@mocks/products';
 
-const getProductsList = async (event: APIGatewayProxyEvent) => {
+const getProductsList = async () => {
   return formatJSONResponse({
     products,
-    event,
   });
 };
 
