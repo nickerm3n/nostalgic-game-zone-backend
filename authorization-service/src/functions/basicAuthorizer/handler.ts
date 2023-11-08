4fc20bee97bcdf7fcd2d183a9121f9b359c6e046
Mykyta_Kurylko <mykyta_kurylko@epam.com>
2023-11-08 20:45:16 +0100

    Task 7 (Authorization)

--- authorization-service/src/functions/basicAuthorizer/handler.ts
+++ authorization-service/src/functions/basicAuthorizer/handler.ts
@@ -2,25 +2,27 @@ import {
   APIGatewayTokenAuthorizerEvent,
   APIGatewayAuthorizerResult,
 } from "aws-lambda";
-import { generatePolicy } from "./helper";
+import { generatePolicy } from "@functions/basicAuthorizer/helper";
 
-const basicAuthorizer = (
+export const basicAuthorizer = async (
   event: APIGatewayTokenAuthorizerEvent,
-): APIGatewayAuthorizerResult => {
+): Promise<APIGatewayAuthorizerResult> => {
   const authorizationToken = event.authorizationToken;
 
-  console.log(`authorizationToken: ${authorizationToken}!`);
+  if (!authorizationToken) {
+    throw new Error("Unauthorized"); // Respond with 401 Unauthorized if no token is present
+  }
 
-  const encodedCreds = authorizationToken.split(" ")[1];
+  const encodedCreds = authorizationToken.replace("Basic ", "");
   const buff = Buffer.from(encodedCreds, "base64");
   const [username, password] = buff.toString("utf-8").split(":");
 
   const expectedPassword = process.env[username];
 
-  if (!username || !password || password !== expectedPassword) {
-    return generatePolicy(encodedCreds, "Deny", event.methodArn);
-  } else {
+  if (password === expectedPassword) {
     return generatePolicy(encodedCreds, "Allow", event.methodArn);
+  } else {
+    return generatePolicy(encodedCreds, "Deny", event.methodArn);
   }
 };
 
